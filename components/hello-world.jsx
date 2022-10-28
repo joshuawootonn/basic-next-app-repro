bc66609dc92a89366fd6cd9c154ab1ded150db10
Joshua Wootonn <josh@makeswift.com>
2022-10-28 16:30:17 -0500

    gaa && gc

---
+++ components/hello-world.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+      export function HelloWorld(props) {
+  return <p {...props}>Hello, world!</p>
+}
--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
+const withMakeswift = require("@makeswift/runtime/next/plugin")();
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-}
-
-module.exports = nextConfig
+  swcMinify: true
+};
+module.exports = withMakeswift(nextConfig);
